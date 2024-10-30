18559bba30f538bb4b780379a55a2edb79581650
haruki <haruki1015go@gmail.com>
2024-10-30 23:17:01 +0900

    [add]ブログページ作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,27 @@
 import Link from "next/link";
 import { client } from "../libs/client";
 
-export default function Home({  }) {
+export default function Home({ blog }) {
   return (
     <div>
-     サンプル
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 // データをテンプレートに受け渡す部分の処理を記述します
-// export const getStaticProps = async () => {
-//   const data = await client.get({ endpoint: "blog" });
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
 
-//   return {
-//     props: {
-//       blog: data.contents,
-//     },
-//   };
-// };
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
