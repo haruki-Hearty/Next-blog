a02b6952e622066fa4baa17b162c7406b7d8116f
haruki <haruki1015go@gmail.com>
2024-11-17 14:28:17 +0900

    [add]型定義追加

--- src/components/blogcard/BlogCard.tsx
+++ src/components/blogcard/BlogCard.tsx
@@ -1,14 +1,18 @@
-// import { MicroCMSImage } from "microcms-js-sdk";
 import { Blog } from "@/types/blog";
+import { NextPage } from "next";
 import Image from "next/image";
 import styles from "./BlogCard.module.scss"
-export const BlogCard = (props) => {
+
+type BlogProps = {
+  blog: Blog;
+}
+
+export const BlogCard: NextPage<BlogProps> = (props) => {
   const { blog } = props;
-  console.log("ブログデータ", blog);
   return (
     <>
       <div className={styles.imgInner}>
-        <Image src={blog.thumbnail.url} width={400} height={300} alt="サンプル" />
+        <Image src={blog.thumbnail.url} width={blog.thumbnail.width} height={blog.thumbnail.height} alt="サンプル" />
       </div>
       <h2 className={styles.title}>{blog.title}</h2>
       <div className={styles.category}>{blog.category.name}</div>
--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,11 +1,18 @@
 import { MicroCMSListContent } from "microcms-js-sdk";
+import { MicroCMSImage } from "microcms-js-sdk";
+
 
 export type Category = {
   name: string;
 };
 
+export type Thumbnail = {
+  url: string;
+};
+
 export type Blog = {
   title: string;
   body: string;
+  thumbnail: Thumbnail & MicroCMSImage;
   category: Category & MicroCMSListContent; //MicroCMSListContentの型定義を&で追加している
 };
